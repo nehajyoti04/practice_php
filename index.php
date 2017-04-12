b6f0a58144fc7b8f383e541d5ef8aa426b957052
nehajyoti <nehajyoti04@gmail.com>
2017-04-12 12:48:41 +0530

    Creating register form

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php
 
+echo "<a href='register.php'>Register</a>";
+echo "<a href='login.php'>Login</a>";
 ?>
-
-<html >
-<head>    
-</head>
-<body>
-
-<!-- Form Code Start -->
-<div id='fg_membersite'>
-<form id='register' action='register_save.php' method='post' accept-charset='UTF-8'>
-
-
-<h2>Register</h2>
-<input type="text" placeholder="Enter Email" name="email" required>
-
-
-</form>
-
-<!--
-Form Code End (see html-form-guide.com for more info.)
--->
-
-</body>
-</html>
---
+++ register.php
@@ -0,0 +1,25 @@
+
+<html >
+<head>    
+</head>
+<body>
+
+<!-- Form Code Start -->
+<div id='fg_membersite'>
+<form id='register' action='register_save.php' method='post' accept-charset='UTF-8'>
+
+
+<h2>Register</h2>
+<input type="text" placeholder="Enter Email" name="email" required>
+<label><b>Password</b></label>
+<input type="password" placeholder="Enter Password" name="psw" required>
+<button type="submit" class="signupbtn">Sign Up</button>
+
+</form>
+
+<!--
+Form Code End (see html-form-guide.com for more info.)
+-->
+
+</body>
+</html>

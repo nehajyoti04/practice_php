2f4fa9edc3695f3dc39e33cea494fb3f55cb9541
nehajyoti <nehajyoti04@gmail.com>
2017-04-12 12:37:30 +0530

    Initializing repo

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<html >
+<head>    
+</head>
+<body>
+
+<!-- Form Code Start -->
+<div id='fg_membersite'>
+<form id='register' action='register_save.php' method='post' accept-charset='UTF-8'>
+
+
+<h2>Register</h2>
+<input type="text" placeholder="Enter Email" name="email" required>
+
+
+</form>
+
+<!--
+Form Code End (see html-form-guide.com for more info.)
+-->
+
+</body>
+</html>
